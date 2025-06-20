fc88e0bf2caeb56d98da20087e5ef50a5213d477
Damien <damien@pc-297.home>
2025-06-20 22:13:06 +0200

    Feature 01 - Add migrations for words and history

---
+++ database/migrations/2025_06_20_193400_create_words_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('chinese_word')->comment('Chinese character');
+            $table->string('pinyin')->comment('Chinese pinyin');
+            $table->string('translation');
+            $table->json('tags');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('words');
+    }
+};
